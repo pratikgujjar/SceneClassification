ddbf1edeae2c51e04d968b7f54eeb7e6f851f8d2
pratikgujjar <pratikgujjar007@gmail.com>
2017-01-27 14:24:09 -0800

    Training 99.45 Validation 84

--- load_data.py
+++ load_data.py
@@ -1,46 +1,52 @@
-from numpy import *
+import numpy as np
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+import cv2
 import os
-from shutil import copyfile, move
 
-# input image dimensions
-img_rows, img_cols = 200, 200
-
-# number of channels
-img_channels = 1
-
-#  Data
-
-source_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope'           #path of folder of images
-train_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_train'     #path of folder to save training images
+train_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_train'    #path of folder to save training images
 test_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_test'      #path of folder to save test images
 
+# load training data
+x_train = []
+y_train = []
+
 listing = os.listdir(train_path)
-num_samples=size(listing)
-print num_samples
+for file in sorted(listing):
+    img = cv2.imread(train_path + "/" + file, cv2.IMREAD_COLOR)
+    x_train.append(img.T)
+    class_num = int(os.path.splitext(file)[0].split('_')[0].encode('utf8'))
+    y_train.append(class_num)
 
-s=8         # Class_number
-count = 1   # Image_number
+y_train[:] = [y - 1 for y in y_train] # Readjusting class numbers to begin from 0
+x_train = np.asarray(x_train) # Change list to numpy array
+y_train = np.asarray(y_train) # Change list to numpy array
 
-for file in listing:
+# load test data
+x_test = []
+y_test = []
 
-    # Code to rename files according to spec: classnumber_classname_imagenumber
+listing = os.listdir(test_path)
+for file in sorted(listing):
+    img = cv2.imread(test_path + "/" + file, cv2.IMREAD_COLOR)
+    x_test.append(img.T)
+    class_num = int(os.path.splitext(file)[0].split('_')[0].encode('utf8'))
+    y_test.append(class_num)
 
-    # class_number = '{:2d}'.format(s)
-    # if ("tallbuilding_") in file:
-    #     print file
-    #     # print fname
-    #     # filename = os.path.splitext(file)[0].split('_')[0].encode('utf8')
-    #     filename = class_number + "_" + "buildings" + "_" + str(count) + ".jpg"
-    #     print filename
-    #     count += 1
-    #     copyfile(source_path + "/" + file, train_path + '/' + filename)
+y_test[:] = [y - 1 for y in y_test] # Readjusting class numbers to begin from 0
+x_test = np.asarray(x_test) # Change list to numpy array
+y_test = np.asarray(y_test) # Change list to numpy array
 
-    # Code to split data into training and testing set.
-    # 250 train images, rest is test
+# normalize inputs from 0-255 to 0.0-1.0
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255.0
+x_test = x_test / 255.0
 
-    img_num = os.path.splitext(file)[0].split('_')[-1].encode('utf8')
-    if int(img_num) > 250:
-        print file
-        move(train_path + "/" + file, test_path + '/' + file)
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
 
-print "Done!"
+print "Image vectors loaded into x_train, y_train, x_test and y_test"
---
+++ net.py
@@ -0,0 +1,138 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+# path of folder with training images
+train_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_train'
+# path of folder with test images
+test_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_test'
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+N_CLASSES = 8
+IM_SIZE = (256,256)
+
+# Create the model
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(3, 256, 256), activation='relu', border_mode='same'))
+model.add(Dropout(0.2))
+model.add(Convolution2D(32, 3, 3, activation='relu', border_mode='same'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(64, 5, 5, activation='relu', border_mode='same'))
+model.add(MaxPooling2D(pool_size=(4,4)))
+model.add(Flatten())
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu', W_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(N_CLASSES, activation='softmax'))
+
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# Show some debug output
+print (model.summary())
+print 'Trainable weights'
+print model.trainable_weights
+
+# Define class mapping
+class_map=['coast', 'forest', 'city', 'buildings', 'highway', 'street', 'country', 'mountain']
+
+# Generate training and testing data
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+train_generator = train_datagen.flow_from_directory(
+    train_path,  # this is the target directory
+    target_size=IM_SIZE,  # all images will be resized to 299x299 Inception V3 input
+    batch_size=8,
+    classes=class_map,
+    class_mode='categorical')
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_generator = test_datagen.flow_from_directory(
+    test_path,  # this is the target directory
+    target_size=IM_SIZE,  # all images will be resized to 256x256
+    batch_size=8,
+    classes=class_map,
+    class_mode='categorical')
+
+model.fit_generator(
+    train_generator,
+    samples_per_epoch=2000,
+    nb_epoch=25,
+    validation_data=test_generator,
+    verbose=2,
+    nb_val_samples=80)
+
+predict_generator = test_datagen.flow_from_directory(
+    test_path,  # this is the target directory
+    target_size=IM_SIZE,  # all images will be resized to 256x256
+    batch_size=2,
+    classes=class_map,
+    class_mode='categorical')
+
+#Save the model
+model_json = model.to_json()
+with open("scene8_model.json", "w") as json_file:
+    json_file.write(model_json)
+
+print "Model saved to disk"
+
+# Save weights
+model.save_weights('scene8_pretrain.h5')  # always save your weights after training or during training
+print "Weights saved to disk"
+
+# model.predict_generator(
+#     predict_generator,
+#     val_samples=2,
+#     max_q_size=10,
+#     pickle_safe=False)
+
+img_path = '/home/pratik/git_projects/ContextAwareness/dataset/envelope_test/coast/1_coast_251.jpg'
+img = image.load_img(img_path, target_size=IM_SIZE)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+preds = model.predict(x)
+print('Predicted:', preds)
+
+
+scores = model.evaluate_generator(
+    predict_generator,
+    val_samples=10,
+    max_q_size=10,
+    nb_worker=1,
+    pickle_safe=False)
+
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+# # Compile model
+# epochs = 25
+# lrate = 0.01
+# decay = lrate/epochs
+# sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# print(model.summary())
+#
+# X_test = x_test[0:32]
+# Y_test = y_test[0:32]
+# np.random.seed(seed)
+# model.fit(x_train, y_train, validation_data=(X_test, Y_test), nb_epoch=epochs, shuffle=True, batch_size=8)
+#
+# # Final evaluation of the model
+# scores = model.evaluate(x_test, y_test, verbose=0)
+# print("Accuracy: %.2f%%" % (scores[1]*100))
+
+# specify number of test images
